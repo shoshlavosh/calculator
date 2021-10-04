6cbca86c086911f6a2a7cc0ff25563ec0a5da8a7
shoshlavosh <shoshlavosh@gmail.com>
2021-10-04 02:44:26 +0000

    arithmetic.py completed

---
+++ arithmetic.py
@@ -0,0 +1,49 @@
+"""Arithmetic Functions"""
+
+
+def add(num1, num2):
+    """Return the sum of two numbers"""
+
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    """Return the difference of two numbers"""
+
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """Return the quotient of two numbers"""
+
+    return num1 / num2
+
+
+def square(num):
+    """Return the square of a number"""
+
+    return num**2
+
+
+def cube(num):
+    """Return the cube of a number"""
+
+    return num**3
+
+
+def power(num, exponent):
+    """Return a number the power of the exponent"""
+
+    return num**exponent
+
+
+def modulo(num1, num2):
+    """Return the remainder of one number divided by another"""
+
+    return num1 % num2
